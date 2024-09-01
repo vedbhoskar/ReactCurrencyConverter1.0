48938188c46bb4a2c35992c3e76c4726d3e7c499
VED BHOSKAR <74398583+vedbhoskar@users.noreply.github.com>
2024-09-01 10:12:13 +0530

    bug fixed

--- currencyConverter2/currencyconverter2.0/src/hooks/useCurrencyInfo.js
+++ currencyConverter2/currencyconverter2.0/src/hooks/useCurrencyInfo.js
@@ -1,16 +1,24 @@
-import { useEffect , useState} from "react";
+import { useEffect, useState } from "react";
 
-function useCurrencyInfo(currency){
-    const [data,setdata] = useState({})
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({});
+
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res)=> res.json())
-        .then((res)=> setdata(res[currency],res[currency]))
-        console.log(data)
-    }, [currency, data])
-    console.log(data)
-    return data
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`) 
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then((res) => setData(res[currency]))
+            .catch((error) => {
+                console.error('Error fetching currency data:', error);
+                setData({});
+            });
+    }, [currency]);
 
+    return data;
 }
 
 export default useCurrencyInfo;
